4f14c53b6ee8ca78713ed97793ab9a1676fe8b6e
munyeol-Yoon <gmakin36@gmail.com>
2022-10-09 15:50:41 +0900

    login bug fixed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,265 +4,313 @@ import fetch from "node-fetch";
 import User from "../models/User";
 
 export const getJoin = (req, res) => {
-    return res.render("join", {pageTitle:"Join"});
+  return res.render("join", { pageTitle: "Join" });
 };
 export const postJoin = async (req, res) => {
-    const {email, username, name, password, password2, location} = req.body;
-    
-    if(password !== password2){
-        return res.status(400).render("join", {pageTitle:"Join", errorMessage:"비밀번호가 틀려요 😓"})
-    }
-    const existsEmail = await User.exists({$or : [{email}]});
-    if(existsEmail){
-        return res.status(400).render("join", {pageTitle:"Join", errorMessage:"이미 같은 이메일이 존재해요 😓"})
-    }
-    const existsUsername = await User.exists({$or : [{username}]});
-    if(existsUsername){
-        return res.status(400).render("join", {pageTitle:"Join", errorMessage:"이미 같은 아이디가 존재해요 😓"})
-    }
-    try{
-        await User.create({
-            email,
-            username,
-            name,
-            password,
-            password2,
-            location,
-        });
-        console.log(req.body);
-        return res.redirect("/login");
-    }catch(error){
-        return res.status(400).render("join", {pageTitle:"400", errorMessage:error._message});
-    }
-}
+  const { email, username, name, password, password2, location } = req.body;
+
+  if (password !== password2) {
+    return res
+      .status(400)
+      .render("join", {
+        pageTitle: "Join",
+        errorMessage: "비밀번호가 틀려요 😓",
+      });
+  }
+  const existsEmail = await User.exists({ $or: [{ email }] });
+  if (existsEmail) {
+    return res
+      .status(400)
+      .render("join", {
+        pageTitle: "Join",
+        errorMessage: "이미 같은 이메일이 존재해요 😓",
+      });
+  }
+  const existsUsername = await User.exists({ $or: [{ username }] });
+  if (existsUsername) {
+    return res
+      .status(400)
+      .render("join", {
+        pageTitle: "Join",
+        errorMessage: "이미 같은 아이디가 존재해요 😓",
+      });
+  }
+  try {
+    await User.create({
+      email,
+      username,
+      name,
+      password,
+      password2,
+      location,
+    });
+    console.log(req.body);
+    return res.redirect("/login");
+  } catch (error) {
+    return res
+      .status(400)
+      .render("join", { pageTitle: "400", errorMessage: error._message });
+  }
+};
 
 export const getLogin = (req, res) => {
-    return res.render("login", {pageTitle:"Login"});
-}
+  return res.render("login", { pageTitle: "Login" });
+};
 
 export const postLogin = async (req, res) => {
-    const {username, password} = req.body;
-    const user = await User.findOne({username, github:false});
-    if(!user){
-        return res.status(400).render("login", {pageTitle:"Login", errorMessage:"아이디가 존재하지 않아요 😓"});
+  const { username, password } = req.body;
+  const user = await User.findOne({ username, github: false });
+  if (!user) {
+    return res
+      .status(400)
+      .render("login", {
+        pageTitle: "Login",
+        errorMessage: "아이디가 존재하지 않아요 😓",
+      });
+  }
+  const compare = await bcrypt.compare(password, user.password);
+  if (!compare) {
+    return res
+      .status(400)
+      .render("login", {
+        pageTitle: "Login",
+        errorMessage: "비밀번호가 틀렸어요 😓",
+      });
+  }
+  req.session.loggedIn = true;
+  req.session.user = user;
+  return res.redirect("/");
+};
+
+export const logout = (req, res) => {
+  req.session.loggedIn = false;
+  req.session.user = null;
+  req.session.destroy();
+  return res.redirect("/");
+};
+
+export const myProfile = (req, res) => {
+  return res.render("myprofile", { pageTitle: "프로필" });
+};
+
+export const getEdit = (req, res) => {
+  return res.render("edit", { pageTitle: "회원정보변경" });
+};
+
+export const postEdit = async (req, res) => {
+  const { email, username, name, location } = req.body;
+  const { _id } = req.session;
+
+  if (email !== req.session.email) {
+    const exists = await User.findOne({ email });
+    if (exists) {
+      return res
+        .status(400)
+        .render("edit", {
+          pageTitle: "회원정보변경",
+          errorMessage: "같은 이메일이 존재합니다.",
+        });
     }
-    const compare = await bcrypt.compare(password, user.password);
-    if(!compare){
-        return res.status(400).render("login", {pageTitle:"Login", errorMessage:"비밀번호가 틀렸어요 😓"});
+  }
+  if (username !== req.session.username) {
+    const exists = await User.findOne({ username });
+    if (exists) {
+      return res
+        .status(400)
+        .render("edit", {
+          pageTitle: "회원정보변경",
+          errorMessage: "같은 아이디가 존재합니다.",
+        });
+    }
+  }
+  const updateUser = await User.findOneAndUpdate(
+    _id,
+    {
+      email,
+      username,
+      name,
+      location,
+    },
+    { new: true }
+  );
+
+  return res.redirect("/users/myprofile");
+};
+
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(userData);
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(emailData);
+    const emailObj = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+    if (!emailObj) {
+      return res.redirect("/login");
+    }
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      const user = await User.create({
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        location: userData.location,
+        github: true,
+      });
     }
     req.session.loggedIn = true;
     req.session.user = user;
     return res.redirect("/");
-}
-
-export const logout = (req, res) => {
-    req.session.loggedIn = false;
-    req.session.user = null;
-    req.session.destroy();
-    return res.redirect("/");
-}
-
-export const myProfile = (req, res) => {
-    return res.render("myprofile", {pageTitle:"프로필"});
-}
-
-export const getEdit = (req, res) => {
-    return res.render("edit", {pageTitle:"회원정보변경"});
-}
-
-export const postEdit = async (req, res) => {
-    const {email, username, name, location} = req.body;
-    const {_id} = req.session;
-
-    if(email !== req.session.email){
-        const exists = await User.findOne({email});
-        if(exists){
-            return res.status(400).render("edit", {pageTitle:"회원정보변경", errorMessage:"같은 이메일이 존재합니다."});
-        }
-    }
-    if(username !== req.session.username){
-        const exists = await User.findOne({username});
-        if(exists){
-            return res.status(400).render("edit", {pageTitle:"회원정보변경", errorMessage:"같은 아이디가 존재합니다."});
-        }
-    }
-    const updateUser = await User.findOneAndUpdate(_id,{
-        email,
-        username,
-        name,
-        location
-    },
-    {new:true}
-    );
-
-    return res.redirect("/users/myprofile");
-}
-
-export const startGithubLogin = (req, res) => {
-    const baseUrl = "https://github.com/login/oauth/authorize";
-    const config = {
-        client_id : process.env.GH_CLIENT,
-        allow_signup: false,
-        scope: "read:user user:email",
-    };
-    const params = new URLSearchParams(config).toString();
-    const finalUrl = `${baseUrl}?${params}`;
-    return res.redirect(finalUrl);
-}
-
-export const finishGithubLogin = async (req, res) => {
-    const baseUrl = "https://github.com/login/oauth/access_token";
-    const config = {
-        client_id:process.env.GH_CLIENT,
-        client_secret: process.env.GH_SECRET,
-        code : req.query.code
-    };
-    const params = new URLSearchParams(config).toString();
-    const finalUrl = `${baseUrl}?${params}`;
-    const tokenRequest = await (
-        await fetch(finalUrl, {
-        method:"POST",
-        headers:{
-            Accept:"application/json",
-        },
-    })).json();
-    if("access_token" in tokenRequest){
-        const {access_token} = tokenRequest;
-        const apiUrl = "https://api.github.com";
-        const userData = await (
-            await fetch(`${apiUrl}/user`, {
-                headers:{
-                    Authorization:`token ${access_token}`,
-                },
-            })
-        ).json();
-        console.log(userData);
-        const emailData = await (
-            await fetch(`${apiUrl}/user/emails`, {
-                headers:{
-                    Authorization:`token ${access_token}`,
-                },
-            })
-        ).json();
-        console.log(emailData);
-        const emailObj = emailData.find(
-            (email) => email.primary === true && email.verified === true
-        );
-        if(!emailObj){
-            return res.redirect("/login");
-        }
-        let user = await User.findOne({email : emailObj.email});
-        if(!user){
-            const user = await User.create({
-                name : userData.name,
-                username : userData.login,
-                email: emailObj.email,
-                password:"",
-                location: userData.location,
-                github : true
-            });
-        }
-        req.session.loggedIn = true;
-        req.session.user = user;
-        return res.redirect("/");
-    }
-    else{
-        return redirect("/login");
-    }
+  } else {
+    return redirect("/login");
+  }
 };
 
-export const startKakaoLogin = (req, res) =>{
-    const baseUrl = "https://kauth.kakao.com/oauth/authorize";
-    const config ={
-        client_id:process.env.KA_CLIENT,
-        redirect_uri:process.env.KA_CALLBACK,
-        response_type:"code"
-    };
-    const params = new URLSearchParams(config).toString();
-    const finalUrl = `${baseUrl}?${params}`;
-    return res.redirect(finalUrl);
-}
+export const startKakaoLogin = (req, res) => {
+  const baseUrl = "https://kauth.kakao.com/oauth/authorize";
+  const config = {
+    client_id: process.env.KA_CLIENT,
+    redirect_uri: process.env.KA_CALLBACK,
+    response_type: "code",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
 
 export const finishKakaoLogin = async (req, res) => {
-    const baseUrl = "https://kauth.kakao.com/oauth/token";
-    const apiUrl = "https://kapi.kakao.com/v2/user/me";
-    const config ={
-        client_id:process.env.KA_CLIENT,
-        client_secret:process.env.KA_SECRET,
-        grant_type:"authorization_code",
-        redirect_uri:process.env.KA_CALLBACK,
-        code:req.query.code,
-    };
-    const params = new URLSearchParams(config).toString();
-    const finalUrl = `${baseUrl}?${params}`;
-    const tokenRequest = await (
-        await fetch(finalUrl, {
-            method:"POST",
-            headers:{
-                "Content-type":"application/json"
-            },
-        })
+  const baseUrl = "https://kauth.kakao.com/oauth/token";
+  const apiUrl = "https://kapi.kakao.com/v2/user/me";
+  const config = {
+    client_id: process.env.KA_CLIENT,
+    client_secret: process.env.KA_SECRET,
+    grant_type: "authorization_code",
+    redirect_uri: process.env.KA_CALLBACK,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+  ).json();
+
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const userRequest = await (
+      await fetch(apiUrl, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-type": "application/json",
+        },
+      })
     ).json();
+    console.log(userRequest);
+    console.log(userRequest.properties);
 
-    if("access_token" in tokenRequest) {
-        const {access_token} = tokenRequest;
-        const userRequest = await (
-            await fetch(apiUrl, {
-                headers:{
-                    Authorization:`Bearer ${access_token}`,
-                    "Content-type":"application/json",
-                },
-            })
-        ).json();
-        console.log(userRequest);
-        console.log(userRequest.properties);
-        
-        const userNickname = userRequest.properties.nickname;
+    const userNickname = userRequest.properties.nickname;
 
-        const user = User.create({
-            email:userNickname,
-            name:userNickname,
-            username:userNickname,
-            password:"",
-            kakao:true
-        });
+    const user = User.create({
+      email: userNickname,
+      name: userNickname,
+      username: userNickname,
+      password: "",
+      kakao: true,
+    });
 
-        req.session.loggedIn = true;
-        req.session.user = user;
-        return res.redirect("/");
-    }
-    else{
-        return res.redirect("/login");
-    }
-}
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
+  } else {
+    return res.redirect("/login");
+  }
+};
 
 export const getChangePassword = (req, res) => {
-    if(req.session.user.github === true){
-        return res.redirect("/");   
-    }
-    if(req.session.user.kakao === true){
-        return res.redirect("/");   
-    }
-    return res.render("password", {pageTitle:"비밀번호변경"});
-}
+  if (req.session.user.github === true) {
+    return res.redirect("/");
+  }
+  if (req.session.user.kakao === true) {
+    return res.redirect("/");
+  }
+  return res.render("password", { pageTitle: "비밀번호변경" });
+};
 
 export const postChangePassword = async (req, res) => {
-    const {
-        body:{password, changePassword, changePasswordConfirm},
-        session:{
-            user:{_id},
-        }
-    } = req;
-    const user = await User.findById(_id);
-    const match = await bcrypt.compare(password, user.password);
-    
-    if(!match){
-        return res.status(400).render("password", {pageTitle:"비밀번호변경", errorMessage:"비밀번호가 틀렸습니다."});
-    }
-    if(changePassword !== changePasswordConfirm){
-        return res.status(400).render("password", {pageTitle:"비밀번호변경", errorMessage:"비밀번호가 일치하지 않습니다."});
-    }
-    user.password = changePassword;
-    await user.save();
-    return res.redirect("/users/logout");
-}
+  const {
+    body: { password, changePassword, changePasswordConfirm },
+    session: {
+      user: { _id },
+    },
+  } = req;
+  const user = await User.findById(_id);
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    return res
+      .status(400)
+      .render("password", {
+        pageTitle: "비밀번호변경",
+        errorMessage: "비밀번호가 틀렸습니다.",
+      });
+  }
+  if (changePassword !== changePasswordConfirm) {
+    return res
+      .status(400)
+      .render("password", {
+        pageTitle: "비밀번호변경",
+        errorMessage: "비밀번호가 일치하지 않습니다.",
+      });
+  }
+  user.password = changePassword;
+  await user.save();
+  return res.redirect("/users/logout");
+};
--- src/middlewares.js
+++ src/middlewares.js
@@ -7,7 +7,7 @@ export const localsMiddleware = (req, res, next) => {
 };
 
 export const protectorMiddleware = (req, res, next) => {
-  if (req.session.loggedIn) {
+  if (!req.session.loggedIn) {
     return next();
   } else {
     return res.redirect("/login");
@@ -15,7 +15,7 @@ export const protectorMiddleware = (req, res, next) => {
 };
 
 export const publicOnlyMiddleware = (req, res, next) => {
-  if (req.session.loggedIn) {
+  if (!req.session.loggedIn) {
     return next();
   } else {
     return res.redirect("/");
